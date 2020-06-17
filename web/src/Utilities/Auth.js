ef346152bf8dc7b25aa2318da5e7dd6555d44997
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-06-17 20:29:38 +0300

    added auth form

---
+++ web/src/Utilities/Auth.js
@@ -0,0 +1,23 @@
+/* Author: Andrieiev Danil | danssg08@gmail.com | https://github.com/DanilAndreev
+   Copyright (C) 2020 */
+import React from 'react'
+
+
+const AuthContext = React.createContext({user: null, token: null});
+
+function AuthProvider(props) {
+    const [user, setUser] = React.useState(null);
+    const [token, setToken] = React.useState(null);
+
+    const isAdmin = () => {
+        return user && !!user.is_admin;
+    };
+
+    return (
+        <AuthContext.Provider value={{user, setUser, token, setToken, isAdmin}} {...props} />
+    );
+}
+
+const useAuth = () => React.useContext(AuthContext);
+
+export {AuthProvider, useAuth}
